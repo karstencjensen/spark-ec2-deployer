034cac776bd160a7eadf61963fc8b6dd9332fd5a
EC2 Default User <ec2-user@ip-172-31-13-202.us-east-2.compute.internal>
2023-12-16 00:28:03 +0000

    adding dependency for java sdk req for pyspark on ec2

--- scripts/config_ec2/download_dependencies.sh
+++ scripts/config_ec2/download_dependencies.sh
@@ -1,7 +1,11 @@
 sudo yum update -y
+sudo dnf update -y # For Amazon Linux 2023 Machines
 
 sudo chown ec2-user:ec2-user /init_setup_assets/spark-ec2-deployer/scripts/env.sh
 source /init_setup_assets/spark-ec2-deployer/scripts/env.sh
+
+# Install Java - required for Pyspark
+sudo dnf install java-17-amazon-corretto-devel
 
 # S3FS
 sudo yum install -y automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-devel
@@ -22,3 +26,5 @@ do
   # Add the jar to spark-defaults.conf
   echo "spark.jars += /usr/local/spark/jars/$jar_name" >> /usr/local/spark/conf/spark-defaults.conf
 done
+
+echo "hi - ugh!"
